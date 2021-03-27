7f1f7bb3af2e6a1741bc88cdc2639ffccb79cb5b
Sunbin Kim <einsbon@gmail.com>
2021-03-27 11:39:28 +0900

    Update tripedal_test_motion.py

--- PybulletSimulation/tripedal_test_motion.py
+++ PybulletSimulation/tripedal_test_motion.py
@@ -1,13 +1,11 @@
 import pybullet as p
-import time
-from time import sleep
 import pybullet_data
 import numpy as np
 import math
 import os
 import csv
 
-import motorController
+from motorController import MotorController
 
 motor_kp = 0.3
 motor_kd = 0.3
@@ -24,36 +22,25 @@ p.setTimeStep(fixedTimeStep)
 p.setPhysicsEngineParameter(numSolverIterations=numSolverIterations)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-p.resetDebugVisualizerCamera(cameraDistance=0.4,
-                             cameraYaw=30,
-                             cameraPitch=-15,
-                             cameraTargetPosition=[-0.05, 0, 0.1])
+p.resetDebugVisualizerCamera(cameraDistance=0.4, cameraYaw=30, cameraPitch=-15, cameraTargetPosition=[-0.05, 0, 0.1])
 planeID = p.loadURDF("plane.urdf")
 p.changeDynamics(planeID, -1, lateralFriction=0.5)
-robotID = p.loadURDF(os.path.abspath(os.path.dirname(__file__)) +
-                     '/model/tripedalRobot.urdf', [0, 0, 0.25],
+robotID = p.loadURDF(os.path.abspath(os.path.dirname(__file__)) + '/model/tripedalRobot.urdf', [0, 0, 0.25],
                      p.getQuaternionFromEuler([0, 0, 0]),
                      useFixedBase=False)
 
-motorsController = motorController.MotorController(robotID, physicsClient,
-                                                   fixedTimeStep, motor_kp,
-                                                   motor_kd, motor_torque,
-                                                   motor_max_velocity)
-print(motorsController.getRevoluteJoint_nameToId())
+motor = MotorController(robotID, physicsClient, fixedTimeStep, motor_kp, motor_kd, motor_torque,
+                                   motor_max_velocity)
+print(motor.getRevoluteJoint_nameToId())
 
-motorsController.setMotorsAngleInFixedTimestepSyncRealTime(
-    [0, 0, 0, 0, 0, 0, 0, 0, 0], 1, 0)
-motorsController.NoCommandSyncRealTime(3, 1)
+motor.setMotorsAngleInFixedTimestepSyncRealTime([0, 0, 0, 0, 0, 0, 0, 0, 0], 1, 0)
+motor.NoCommandSyncRealTime(3, 1)
 
-f = open(os.path.abspath(os.path.dirname(__file__)) + '/testMotion.CSV',
-         'r',
-         encoding='utf-8')
+f = open(os.path.abspath(os.path.dirname(__file__)) + '/testMotion.CSV', 'r', encoding='utf-8')
 rdr = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
 for line in rdr:
-    print(list(np.multiply(line[0:9], (math.pi / 180))), line[9] / 1000,
-          line[10] / 1000)
-    motorsController.setMotorsAngleInFixedTimestepSyncRealTime(
-        np.multiply(line[0:9], (math.pi / 180)), line[9] / 1000,
-        line[10] / 1000, 1.0)
+    print(list(np.multiply(line[0:9], (math.pi / 180))), line[9] / 1000, line[10] / 1000)
+    motor.setMotorsAngleInFixedTimestepSyncRealTime(np.multiply(line[0:9], (math.pi / 180)), line[9] / 1000,
+                                                               line[10] / 1000, 1.0)
 
-motorsController.NoCommandSyncRealTime(10, 1)
+motor.NoCommandSyncRealTime(10, 1)
